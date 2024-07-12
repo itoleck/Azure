6db9af146284aaba692feaf016e7d7e3e121e5b6
Chad Schultz <25645350+itoleck@users.noreply.github.com>
2024-07-12 14:22:20 -0500

    updates

--- Entra/ApplicationSecretExpirationSolution/Monitor-EntraAppSecretExpirations_MSGraphEmail.ps1
+++ Entra/ApplicationSecretExpirationSolution/Monitor-EntraAppSecretExpirations_MSGraphEmail.ps1
@@ -6,7 +6,7 @@
 
 using namespace System.Collections.Generic
 
-##requires -Modules Microsoft.Graph,Microsoft.Graph.Applications,Microsoft.Graph.Authentication
+##requires -Modules Microsoft.Graph,Microsoft.Graph.Applications,Microsoft.Graph.Authentication,Microsoft.Graph.Mail
 
 #Run in Azure Automation
 # Param(
