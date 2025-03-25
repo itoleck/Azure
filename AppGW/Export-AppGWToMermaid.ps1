a09b6db9612d5f5c6894034c3f1c9d16fb0af97f
Chad Schultz <25645350+itoleck@users.noreply.github.com>
2025-03-25 12:42:26 -0500

    update to AppGW tools

--- AppGW/Export-AppGWToMermaid.ps1
+++ AppGW/Export-AppGWToMermaid.ps1
@@ -23,8 +23,8 @@ function addAppGWConfig {
 }
 
 function addFrontendConfig {
-    $public = $AppGW.FrontendIPConfigurations.Name[0] + "`n" + $AppGW.FrontendPorts.Port
-    $private = $AppGW.FrontendIPConfigurations.Name[1] + "`n" + $AppGW.FrontendPorts.Port
+    $public = $AppGW.FrontendIPConfigurations.Name[0] + "<br />" + $AppGW.FrontendPorts.Port
+    $private = $AppGW.FrontendIPConfigurations.Name[1] + "<br />" + $AppGW.FrontendPorts.Port
     $Script:mermaid += "AppGWPublic[Public<br />$public] --> AppGWListeners[Listeners];`n"
     $Script:mermaid += "AppGWPrivate[Private<br />$private] --> AppGWListeners[Listeners];`n"
 }
@@ -32,13 +32,42 @@ function addFrontendConfig {
 function addListenerConfig {
     foreach($listener in $AppGW.HttpListeners){
         $cert = $listener.SslCertificate.Id.Split("/")[-1]
-        $Script:mermaid += "AppGWListeners[Listeners] --> $($listener.Name)[$($listener.Name)<br />Protocol: $($listener.Protocol)<br />Hostname/s: $($listener.HostName)<br />$($listener.HostNames)<br />Certificate: $cert<br />Provisioning: $($listener.ProvisioningState)];`n"
+        $Script:mermaid += "AppGWListeners[Listeners] --> $($listener.Name){{$($listener.Name)<br />Protocol: $($listener.Protocol)<br />Hostname/s: $($listener.HostName)<br />$($listener.HostNames)<br />Certificate: $cert<br />Provisioning: $($listener.ProvisioningState)}};`n"
     }
 }
 
 function addRulesConfig {
+    $redirect = ""
+
     foreach($rule in $AppGW.RequestRoutingRules){
-        $Script:mermaid += "$($rule.HttpListener.Id.Split("/")[-1]) --> $($rule.Name)[$($rule.Name)<br />$($rule.RuleType)<br />$($rule.Priority)];`n"
+        if ($rule.RedirectConfiguration.Id.Length -gt 0) {
+            $redirect = "$($rule.RedirectConfiguration.Id.Split("/")[-1])"
+            $Script:mermaid += "$($rule.HttpListener.Id.Split("/")[-1]) --> $($rule.Name)[$($rule.Name)<br />Type: $($rule.RuleType)<br />Priority: $($rule.Priority)<br />Redirect: $redirect];`n"
+            $Script:mermaid += "$($rule.Name) --> redirectConfigurations$redirect[$redirect];`n"
+
+            $redirectConfig = $AppGW.RedirectConfigurations | Where-Object {$_.Id -eq $rule.RedirectConfiguration.Id}
+            $targetListener = $redirectConfig.TargetListener.Id.Split("/")[-1]
+            if ($targetListener.Length -eq 0) {
+                $targetListener = $redirectConfig.TargetUrl
+            }
+            $Script:mermaid += "redirectConfigurations$redirect[$redirect] --> $targetListener;`n"
+        } else {
+            $Script:mermaid += "$($rule.HttpListener.Id.Split("/")[-1]) --> $($rule.Name)[$($rule.Name)<br />Type: $($rule.RuleType)<br />Priority: $($rule.Priority)<br />$($rule.BackendHttpSettings.Id.Split("/")[-1])];`n"
+            foreach($beSetting in $AppGW.BackendHttpSettingsCollection){
+                $Script:mermaid += "$($beSetting.Name)([$($beSetting.Name)<br />Port: $($beSetting.Port)<br />Protocol: $($beSetting.Protocol)<br />CookieBasedAffinity: $($beSetting.CookieBasedAffinity)<br />$($beSetting.ProvisioningState)]) --> $($rule.Name)[$($rule.Name)];`n"
+                
+                if ($beSetting.Probe.Id.Length -gt 0) {
+                    $probe = $AppGW.Probes | Where-Object {$_.Id -eq $beSetting.Probe.Id}
+                    $Script:mermaid += "$($probe.Name) -->|$($rule.Name)| $($beSetting.Name);`n"
+                } else {
+                    $Script:mermaid += "Probe[Default Probe] -->|$($rule.Name)| $($beSetting.Name);`n"
+                }
+            }
+            foreach($bePool in $AppGW.BackendAddressPools){
+                $Script:mermaid += "$($bePool.Name)>$($bePool.Name)<br />$($bePool.BackendAddresses)<br />$($bePool.BackendIpConfigurations.Id)] --> $($rule.Name)[$($rule.Name)];`n"
+            }
+        }
+        $redirect = ""
     }
 }
 
@@ -54,10 +83,12 @@ $Script:mermaid += "Loc[Location:<br />$($AppGW.Location)] --> AppGW;`n"
 $http2 = "HTTP2: False";if ($AppGW.EnableHttp2){$http2 = "HTTP2: True"}
 $fips = "FIPS: False";if ($AppGW.EnableFips){$fips = "FIPS: True"}
 $zones = "Zones: False";if ($AppGW.Zones){$zones = "Zones: True"}
+
 addAppGWConfig
 addFrontendConfig
 addListenerConfig
 addRulesConfig
+
 $Script:mermaid += "`n"
 
 Write-Output $Script:mermaid
